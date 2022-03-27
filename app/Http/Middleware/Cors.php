f9ba09dcb9e29a2e5b1921ebc1ad258ef108763c
BenHuang <benhuang0857@gmail.com>
2022-03-28 00:50:44 +0800

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,9 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Origin', $request->server('HTTP_ORIGIN'))
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+			    ->header('Access-Control-Allow-Credentials', 'true')
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
