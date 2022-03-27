a26fc9113f35fc8adf83a701ac5e6caf5691e456
BenHuang <benhuang0857@gmail.com>
2022-03-28 00:57:38 +0800

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,10 +15,27 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-                ->header('Access-Control-Allow-Origin', $request->server('HTTP_ORIGIN'))
-                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
-			    ->header('Access-Control-Allow-Credentials', 'true')
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        // return $next($request)
+        //         ->header('Access-Control-Allow-Origin', $request->server('HTTP_ORIGIN'))
+        //         ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+		// 	    ->header('Access-Control-Allow-Credentials', 'true')
+        //         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+        header("Access-Control-Allow-Origin: *");
+
+        // ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
+        ];
+        if($request->getMethod() == "OPTIONS") {
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return Response::make('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach($headers as $key => $value)
+            $response->header($key, $value);
+        return $response;
     }
 }
