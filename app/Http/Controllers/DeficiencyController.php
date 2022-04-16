0ee7e23161e616a5f1f1cb326ffab7e16577bf1f
BenHuang <benhuang0857@gmail.com>
2022-04-17 00:48:28 +0800

    20220417

---
+++ app/Http/Controllers/DeficiencyController.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DeficiencyController extends Controller
+{
+    public function getChart1(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart2(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart3(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart4(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart5(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart6(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart7(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart8(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart9(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart10(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+    
+    public function getChart11(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart12(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart13(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart14(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart15(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart16(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart17(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart18(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart19(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart20(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart21(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart22(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart23(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart24(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart25(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+
+    public function getChart26(Request $req)
+    {
+        $Data = ['OK'];
+        return json_encode($Data);
+    }
+}
