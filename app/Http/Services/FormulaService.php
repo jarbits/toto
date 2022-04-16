6667f5548aee1ecdaafc0bafc8f942bf1356bca3
BenHuang <benhuang0857@gmail.com>
2022-04-17 05:29:33 +0800

    20220417

--- app/Http/Services/FormulaService.php
+++ app/Http/Services/FormulaService.php
@@ -19,22 +19,22 @@ class FormulaService
                 ";
         }
         if ($Category != null) {
-            // $qStr = "
-            //     AND T.s_category = '".$Category."'
-            //     ";
             $qStr = "
-                ND T.s_person LIKE '%".$Category."%'
+                AND T.s_category = '".$Category."'
                 ";
+            // $qStr = "
+            //     ND T.s_person LIKE '%".$Category."%'
+            //     ";
         }
         if ($Category != null && $Person != null) {
-            // $qStr = "
-            //     AND T.s_category = '".$Category."'
-            //     AND T.s_person = '".$Person."'
-            //     ";
             $qStr = "
-                AND T.s_person = '".$Category."'
-                AND T.s_person LIKE '%".$Person."%'
+                AND T.s_category = '".$Category."'
+                AND T.s_person = '".$Person."'
                 ";
+            // $qStr = "
+            //     AND T.s_person = '".$Category."'
+            //     AND T.s_person LIKE '%".$Person."%'
+            //     ";
         }
         return $qStr;
     }
