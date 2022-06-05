e8d3291110ccb239534fbab657ff14910537770a
BenHuang <benhuang0857@gmail.com>
2022-06-06 00:50:20 +0800

    20220605

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -575,7 +575,7 @@ class APIController extends Controller
         $EndMonth = date('m', strtotime($req->EndTime));
 
         $D1 = date('Y-m-d', strtotime($NowYear.'-'.($NowMonth).'-01 00:00:00'));
-        $D2 = date('Y-m-d', strtotime($NowYear.'-'.($EndMonth).'-01 00:00:00'));
+        $D2 = date('Y-m-d', strtotime($NowYear.'-'.($EndMonth+1).'-01 00:00:00'));
 
         $Qs = $this->FormulaService->getAllQSet(
             $D1, 
