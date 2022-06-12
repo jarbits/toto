d640166e4d755eaa1cb6b60cd0560203496f7e13
BenHuang <benhuang0857@gmail.com>
2022-06-12 22:17:48 +0800

    20220612

--- app/Http/Controllers/SendNumController.php
+++ app/Http/Controllers/SendNumController.php
@@ -9,23 +9,23 @@ class SendNumController extends Controller
 {
     public function update(Request $req)
     {
-        $_yearMonth = $req->yearmonth;
-        $_num = $req->num;
-        $snedNum = SendNum::where('yearmonth', $_yearMonth)->firstOrFail();
+        // $_yearMonth = $req->yearmonth;
+        // $_num = $req->num;
+        // $snedNum = SendNum::where('yearmonth', $_yearMonth)->firstOrFail();
 
-        if($snedNum != null)
-        {
-            $snedNum->num = $req->num;
-            $snedNum->save();
-        }
-        else 
-        {
-            $snedNum = new SendNum();
-            $snedNum->yearMonth = $_yearMonth;
-            $snedNum->num = $_num;
+        // if($snedNum != null)
+        // {
+        //     $snedNum->num = $req->num;
+        //     $snedNum->save();
+        // }
+        // else 
+        // {
+        //     $snedNum = new SendNum();
+        //     $snedNum->yearMonth = $_yearMonth;
+        //     $snedNum->num = $_num;
 
-            $snedNum->save();
-        }
+        //     $snedNum->save();
+        // }
 
         dd(123);
     }
