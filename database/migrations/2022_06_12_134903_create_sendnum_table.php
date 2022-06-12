23c7f3388e66b97aefebac3edc1619746d3ebc1d
BenHuang <benhuang0857@gmail.com>
2022-06-12 22:12:44 +0800

    20220612

--- database/migrations/2022_06_12_134903_create_sendnum_table.php
+++ database/migrations/2022_06_12_134903_create_sendnum_table.php
@@ -16,7 +16,7 @@ class CreateSendnumTable extends Migration
         Schema::create('sendnum', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('num');
-            $table->datatime('yearmonth');
+            $table->datetime('yearmonth');
             $table->timestamps();
         });
     }
