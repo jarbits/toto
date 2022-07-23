23e3358a6e4c5d3c1fbf3a48a13566554c51a993
BenHuang <benhuang0857@gmail.com>
2022-07-23 14:43:04 +0800

    20220723

--- app/Http/Controllers/SummaryController.php
+++ app/Http/Controllers/SummaryController.php
@@ -423,21 +423,19 @@ class SummaryController extends Controller
         foreach($Table as $item)
         {
             $rq14Set = $item->rq14;
-            if ($rq14Set != null) {
-                try {
-                    $rq14Set = explode(',', $rq14Set);
-                } catch (\Throwable $th) {}
-                
-                $rq14IntSet = array();
-                foreach ($rq14Set as $rq14) 
-                {
-                    array_push($rq14IntSet, intval($rq14));
-                }
-    
-                $theMaxVal = max($rq14IntSet);
-                if ($theMaxVal <= 999) {
-                    array_push($PraiseData, $item);
-                }
+            try {
+                $rq14Set = explode(',', $rq14Set);
+            } catch (\Throwable $th) {}
+            
+            $rq14IntSet = array();
+            foreach ($rq14Set as $rq14) 
+            {
+                array_push($rq14IntSet, intval($rq14));
+            }
+
+            $theMaxVal = max($rq14IntSet);
+            if ($theMaxVal <= 999) {
+                array_push($PraiseData, $item);
             }
         }
 
