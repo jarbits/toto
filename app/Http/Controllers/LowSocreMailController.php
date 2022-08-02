ed842c38eabdbf152bb1254831562388e6c3010e
BenHuang <benhuang0857@gmail.com>
2022-08-02 22:49:00 +0800

    Mail setting 20220802

--- app/Http/Controllers/LowSocreMailController.php
+++ app/Http/Controllers/LowSocreMailController.php
@@ -10,23 +10,23 @@ class LowSocreMailController extends Controller
 {
     public function send()
     {
-        // $to = collect([
-        //     ['name' => 'Jason', 'email' => 'benhuang0857@gmail.com']
-        // ]);
+        $to = collect([
+            ['name' => 'Ben', 'email' => 'benhuang0857@gmail.com']
+        ]);
  
-        // // 提供給模板的參數
-        // $params = [
-        //     'say' => '您好，這是一段測試訊息的內容'
-        // ];
+        // 提供給模板的參數
+        $params = [
+            'say' => '您好，這是一段測試訊息的內容'
+        ];
  
-        // // 若要直接檢視模板
-        // // echo (new Warning($data))->render();die;
+        // 若要直接檢視模板
+        // echo (new Warning($data))->render();die;
  
-        // Mail::to($to)->send(new LowScoreMail($params));
+        Mail::to($to)->send(new LowScoreMail($params));
 
-        Mail::raw('測試使用 Laravel 5 的 Mailgun 寄信服務', function($message)
-        {
-            $message->to('benhuang0857@gmail.com');
-        });
+        // Mail::raw('測試使用 Laravel 5 的 Mailgun 寄信服務', function($message)
+        // {
+        //     $message->to('benhuang0857@gmail.com');
+        // });
     }
 }
