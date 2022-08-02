d31509f2d5842a21f5aa59c45560b1f89fbaa6ba
BenHuang <benhuang0857@gmail.com>
2022-08-02 20:43:58 +0800

    Mail setting 20220802

--- app/Http/Controllers/LowSocreMailController.php
+++ app/Http/Controllers/LowSocreMailController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class LowSocreMailController extends Controller
 {
-    public function ship()
+    public function send()
     {
         Mail::to("benhuang0857@gmail.com")
           ->send("123321");
