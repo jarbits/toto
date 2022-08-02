ee344fc277138a4e52ec608114ee4f781af01ac7
BenHuang <benhuang0857@gmail.com>
2022-08-02 20:48:05 +0800

    Mail setting 20220802

--- app/Http/Controllers/LowSocreMailController.php
+++ app/Http/Controllers/LowSocreMailController.php
@@ -9,8 +9,18 @@ class LowSocreMailController extends Controller
 {
     public function send()
     {
-        Mail::to("benhuang0857@gmail.com")
-          ->send("123321");
-
+        $to = collect([
+            ['name' => 'Jason', 'email' => 'benhuang0857@gmail.com']
+        ]);
+ 
+        // 提供給模板的參數
+        $params = [
+            'say' => '您好，這是一段測試訊息的內容'
+        ];
+ 
+        // 若要直接檢視模板
+        // echo (new Warning($data))->render();die;
+ 
+        Mail::to($to)->send("HI");
     }
 }
