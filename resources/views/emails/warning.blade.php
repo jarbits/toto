44206ce8a8b15277063dde83cb37afc8532ca4a7
BenHuang <benhuang0857@gmail.com>
2022-08-10 21:43:00 +0800

    20220810 Mail

--- resources/views/emails/warning.blade.php
+++ resources/views/emails/warning.blade.php
@@ -5,7 +5,7 @@
         <th>示警日期</th>
         <th>客服中心</th>
         <th>售服員</th>
-        <th>客訴項目</th>
+        <!-- <th>客訴項目</th> -->
         <th>最低評分</th>
         <th>整體評分</th>
         <th>推薦意願</th>
@@ -17,7 +17,7 @@
         <td>{{ $params['t3'] }}</td>
         <td>{{ $params['t4'] }}</td>
         <td>{{ $params['t5'] }}</td>
-        <td>{{ $params['t6'] }}</td>
+        <!-- <td>{{ $params['t6'] }}</td> -->
         <td>{{ $params['t7'] }}</td>
         <td>{{ $params['t8'] }}</td>
         <td>{{ $params['t9'] }}</td>
