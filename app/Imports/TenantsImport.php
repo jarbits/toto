8b6720805b30ede7d114249d8765d7cf2de36bcb
BenHuang <benhuang0857@gmail.com>
2022-08-10 21:18:36 +0800

    20220810 Mail

--- app/Imports/TenantsImport.php
+++ app/Imports/TenantsImport.php
@@ -93,7 +93,7 @@ class TenantsImport implements ToModel,WithStartRow
                 || ($q13 != null && intval($q13) <= 4) ) 
                 {
 
-                    $users = Mail2User::where('s_region', $row[34])->get();
+                    $users = Mail2User::where('s_region', '北區')->get();
                         if ($users != null) {
                             foreach ($users as $user) {
                                 
